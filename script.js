72509787dcf5dfc57517dd92ca84a0aa70e38b50
wasayabdul <wasay54@hotmail.com>
2024-07-11 22:37:28 -0400

    Cursor blinking for title

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 
 const text = "hi, Wasay here!";
 let index = 0;
+
+function type() {
+    if (index < text.length) {
+        document.getElementById("typing").textContent += text.charAt(index);
+        index++;
+        setTimeout(type, 150);
+    } else {
+        document.querySelector(".cursor").classList.add('blinking');
+    }
+}
+
+document.addEventListener("DOMContentLoaded", type);
